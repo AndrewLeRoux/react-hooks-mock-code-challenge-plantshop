4ab7921786c80272c7a4c2ad457b4a2cf5460ba6
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-02-18 13:25:49 -0800

    commit tree 5b0c0b4e64e286a3bc831540fea29fb38e30a7fb

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    fetch ('http://localhost:6001/plants')
+    .then(r => r.json())
+    .then(data => {setPlants(data)})
+  }, [])
+
+  function handleAddPlant(newPlant){
+    setPlants([...plants, newPlant])
+  }
+
+  
+  const updatedPlants = plants.filter(plant => plant.name.toLowerCase().includes(input.toLowerCase()))
+  
+
+  
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onSubmit = {handleAddPlant}/>
+      <Search input = {input} onSearch= {setInput}/>
+      <PlantList plants = {updatedPlants}/>
     </main>
   );
 }
